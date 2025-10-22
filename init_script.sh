2d37164ede11dee7a523c31c988bc824fbf75e6a
sojeba008 <sojeba008@gmail.com>
2025-10-23 00:48:13 +0200

    decommission: stop sending trains data to Kafka and Delta Lake

--- init_script.sh
+++ init_script.sh
@@ -1,8 +1,2 @@
 mkdir -p ./airflow/logs
-mkdir postgres-db-volume
-mkdir -p ./airflow/dags
 sudo chmod -R 777 ./airflow/*
-# mkdir local-delta
-# sudo chown -R 50000:0 ./airflow/dags
-#docker build -t custom-airflow Dockerfiles/Airflow/
-#docker build -t delta-kafka-consumer Dockerfiles/Spark/
